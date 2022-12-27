d5425a6a41e9ace45d4bd442c0500c9f34c512ca
Jasper Alblas <just_jasper@hotmail.com>
2022-12-27 21:38:02 +0100

    Fetched data is now added to csv

--- script.py
+++ script.py
@@ -1,26 +1,48 @@
-#import pyodbc
+# import pyodbc
 import configparser
+
 import requests
-
-config = configparser.ConfigParser()
-config.read('config.conf')
-
-print(config['DEFAULT']['url'])
+import json
+import csv
 
 
-url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
+def fetchData():
+    try:
+        config = configparser.ConfigParser()
+        config.read('config.conf')
 
-response = requests.get(url)
+        # print(config['DEFAULT']['url'])
 
-if response:
-    json = response.json()
-    features = json['features']
+        url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
 
-    for feature in features:
-        print(feature)
-        print("==================================")
+        response = requests.get(url)
 
-else:
-    print('An error has occurred.')
+        if response:
+            json = response.json()
+            features = json['features']
+            all_earthquakes = []
+
+            for feature in features:
+                current_earthquake = []
+                print(feature)
+                print("==================================")
+                current_earthquake.append(feature['properties']['mag'])
+                current_earthquake.append(
+                    feature['geometry']['coordinates'][0])
+                current_earthquake.append(
+                    feature['geometry']['coordinates'][1])
+
+                all_earthquakes.append(current_earthquake)
+
+            csv_file = 'earthquakes.csv'
+
+            with open(csv_file, 'w') as fp:
+                csv_writer = csv.writer(fp, delimiter='|')
+                csv_writer.writerows(all_earthquakes)
+        else:
+            print('An error has occurred.')
+    except:
+        print('A error occured!')
 
 
+fetchData()
